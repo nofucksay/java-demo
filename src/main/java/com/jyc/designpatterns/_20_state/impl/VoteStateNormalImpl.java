88b9b7dcae259b733920e557252d5c88ddb53060
nofucksay <13269250702@163.com>
2017-06-01 00:02:17 +0800

    [新增] 1.添加[设计模式之状态(对象)模式]的demo

---
+++ src/main/java/com/jyc/designpatterns/_20_state/impl/VoteStateNormalImpl.java
@@ -0,0 +1,20 @@
+package com.jyc.designpatterns._20_state.impl;
+
+import com.jyc.designpatterns._20_state.VoteManager;
+import com.jyc.designpatterns._20_state.VoteState;
+
+/**
+ * TODO 正常状态的处理器
+ * @author 贾玉晨
+ * @date 2017年5月31日-下午11:39:19	 
+ */
+public class VoteStateNormalImpl implements VoteState{
+
+	@Override
+	public void doVote(String voterName, String voteItem, VoteManager vm) {
+		// 进行投票
+		vm.voteItemMap.put(voterName, voteItem);
+		System.out.println("恭喜,投票成功!");
+	}
+	
+}
